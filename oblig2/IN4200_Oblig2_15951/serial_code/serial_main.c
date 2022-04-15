ffd5144cbb203331cf67ccd4db434b0d14302a92
Mikkel Metzsch Jensen <metzschjensen@gmail.com>
2022-04-15 22:09:16 +0200

    serial code more or less complete

--- oblig2/IN4200_Oblig2_15951/serial_code/serial_main.c
+++ oblig2/IN4200_Oblig2_15951/serial_code/serial_main.c
@@ -6,6 +6,8 @@
 #include <malloc.h>
 #endif
 
+#include "functions.h"
+
 /* needed header files .... */
 /* declarations of functions import_JPEG_file and export_JPEG_file */
 
@@ -17,12 +19,11 @@ void export_JPEG_file (const char* filename, const unsigned char* image_chars,
                        int num_components, int quality);
 
 
-
 int main(int argc, char *argv[])
 {
     int m, n, c, iters;
     float kappa;
-    // image u, u_bar;
+    image u, u_bar;
     unsigned char *image_chars;
     char *input_jpeg_filename, *output_jpeg_filename;
    
@@ -31,22 +32,29 @@ int main(int argc, char *argv[])
     /* temporary hardcoded inputs */
     kappa = 0.2;
     iters = 10;
-    input_jpeg_filename  = 'toys.jpg';
-    output_jpeg_filename = 'processed_image.jpg';
+    // input_jpeg_filename = "mona_lisa_noisy.jpg";
+    input_jpeg_filename = "gray.jpg";
 
-    int height, width, comp;
-    import_JPEG_file("toys.jpg", &image_chars, &height, &width, &comp);
-    printf("Succeeded! vertical pixels: %d, horizontal pixels: %d, num components: %d\n", height, width, comp);
+    output_jpeg_filename = "image_out.jpg";
 
-    // import_JPEG_file(input_jpeg_filename, &image_chars, &m, &n, &c);
-    // allocate_image (&u, m, n);
-    // allocate_image (&u_bar, m, n);
-    // convert_jpeg_to_image (image_chars, &u);
-    // iso_diffusion_denoising (&u, &u_bar, kappa, iters);
-    // convert_image_to_jpeg (&u_bar, image_chars);
-    // export_JPEG_file(output_jpeg_filename, image_chars, m, n, c, 75);
-    // deallocate_image (&u);
-    // deallocate_image (&u_bar);
+    import_JPEG_file(input_jpeg_filename, &image_chars, &m, &n, &c);
+    printf("Succeeded! vertical pixels: %d, horizontal pixels: %d, num components: %d\n", m, n, c);
+
+
+    allocate_image (&u, m, n);
+    allocate_image (&u_bar, m, n);
+    convert_jpeg_to_image (image_chars, &u);
+
+    iso_diffusion_denoising (&u, &u_bar, kappa, iters);
+
+    printf("%f\n", u.image_data[5][5]);
+    printf("%f\n", u_bar.image_data[5][5]);
+
+
+    convert_image_to_jpeg(&u_bar, image_chars);
+    export_JPEG_file(output_jpeg_filename, image_chars, m, n, c, 75);
+    deallocate_image (&u);
+    deallocate_image (&u_bar);
         return 0;
 }
 
